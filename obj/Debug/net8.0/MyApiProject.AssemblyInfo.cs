e010a1eee27ac08c21ad86627369e11d12bcfc0e
keitoHeng <113132142+keitoHeng@users.noreply.github.com>
2025-07-02 12:26:27 +0700

    Update frontend

--- obj/Debug/net8.0/MyApiProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyApiProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyApiProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5abb640b3f975e9481c92870d0645c1982e9b5fc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e4a7d0b0c4f13fa380abd3b184aeb45d3e3e2c9")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyApiProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyApiProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
